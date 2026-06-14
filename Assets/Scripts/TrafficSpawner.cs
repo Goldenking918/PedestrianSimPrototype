a2726e8e8c4d8cdf6b4efc0d0f5350e9a3c7386e
Miles Makino-Torrington <mmak111@aucklanduni.ac.nz>
2026-06-14 23:54:31 +1200

    Add variation and speed and spawn

--- Assets/Scripts/TrafficSpawner.cs
+++ Assets/Scripts/TrafficSpawner.cs
@@ -8,10 +8,34 @@ public class TrafficSpawner : MonoBehaviour
     public float spawnInterval = 3f;
     [Tooltip("Minimum radius around spawner that must be clear to spawn a new car")]
     public float spawnClearRadius = 2f;
+    [Tooltip("Minimum spawn interval (seconds)")]
+    public float spawnIntervalMin = 1.0f;
+    [Tooltip("Maximum spawn interval (seconds)")]
+    public float spawnIntervalMax = 5.0f;
+
+    [Tooltip("Minimum random speed assigned to spawned cars")]
+    public float spawnCarMinSpeed = 6f;
+    [Tooltip("Maximum random speed assigned to spawned cars")]
+    public float spawnCarMaxSpeed = 12f;
 
     void Start()
     {
-        InvokeRepeating(nameof(SpawnCar), 0f, spawnInterval);
+        StartCoroutine(SpawnLoop());
+    }
+
+    private System.Collections.IEnumerator SpawnLoop()
+    {
+        while (true)
+        {
+            float interval = spawnInterval;
+            if (spawnIntervalMax > spawnIntervalMin)
+            {
+                interval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+            }
+
+            SpawnCar();
+            yield return new WaitForSeconds(interval);
+        }
     }
 
     void SpawnCar()
@@ -30,6 +54,11 @@ public class TrafficSpawner : MonoBehaviour
         GameObject car = Instantiate(carPrefab, transform.position, transform.rotation);
         var cm = car.GetComponent<CarMovement>();
         if (cm != null)
+        {
             cm.waypoints = waypoints;
+            // assign randomized speed
+            if (spawnCarMaxSpeed > spawnCarMinSpeed)
+                cm.speed = Random.Range(spawnCarMinSpeed, spawnCarMaxSpeed);
+        }
     }
 }
